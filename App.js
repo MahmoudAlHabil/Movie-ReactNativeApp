7abe36da4a91a5ad2a11aa1cf89d7e894afd7718
Mahmoud AlHabil <mahmoudalhabil2001@gmail.com>
2022-10-28 23:04:48 +0300

    Add push notifications with firebase

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import auth from '@react-native-firebase/auth';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/navigation';
 import HomeDrawer from './src/navigation/HomeDrawer';
+import messaging from '@react-native-firebase/messaging';
+
 
 const App = () => {
   const [initializing, setInitializing] = useState(true);
@@ -15,19 +17,33 @@ const App = () => {
   }
 
   useEffect(() => {
+
+    (async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        messaging().getToken().then((val) => console.log(val))
+      }
+    })()
+  }, [])
+
+  useEffect(() => {
     SplashScreen.hide();
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; 
+    return subscriber;
   }, []);
 
   if (initializing) return null;
 
   if (!user) {
-    return (<Navigation/>
+    return (<Navigation />
     );
   }
 
-  return (<NavigationContainer><HomeDrawer/></NavigationContainer>);
+  return (<NavigationContainer><HomeDrawer /></NavigationContainer>);
 }
 
 export default App;
